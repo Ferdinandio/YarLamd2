bc27ed0d8c825f7668faf594337fffafb0171d6d
Ferdinandio <dmi8829@yandex.ru>
2020-04-19 21:17:40 +0400

    last

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener("click", () => {
+
+
+hamburger.addEventListener("click", function() {
   navLinks.classList.toggle("open");
-  links.forEach(link => {
-    link.classList.toggle("fade");
-  });
+  // links.forEach(function link() {
+  //   link.classList.toggle("fade");
+  // });
+  for (var link in links) {
+    link.classList.toggle("open");
+  }
 });
 
 
@@ -16,3 +21,21 @@ hamburger.addEventListener("click", () => {
 $(function() {
   $("#phone").mask("+7(999) 999-99-99");
 });
+
+
+
+// let btnServe = document.querySelector('#btnServe');
+// let btnGal = document.querySelector('#btnGal');
+// let ourServe = document.querySelector('.sec1');
+// let gal = document.querySelector('.gal');
+// console.log(btnServe);
+
+
+
+// btnServe.addEventListener('click', function() {
+//   document.body.scrollTop = ourServe.scrollIntoView(top);
+// });
+
+// btnGal.addEventListener('click', function() {
+//   document.body.scrollTop = btnGal.scrollIntoView(top);
+// });
