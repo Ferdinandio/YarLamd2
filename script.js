c16ca7d1ba524157fe6c66394216f47f467e0a1d
Ferdinandio <dmi8829@yandex.ru>
2020-04-27 20:59:02 +0400

    fix

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-// const hamburger = document.querySelector(".hamburger");
-// const navLinks = document.querySelector(".nav-links");
-// const links = document.querySelectorAll(".nav-links li");
-// const line = document.querySelectorAll(".line");
-
-
-
-
-// hamburger.addEventListener("click", function() {
-//   navLinks.classList.toggle("open");
-//   links.forEach(function link() {
-//     link.classList.toggle("fade");
-//   });
-// });
-
-
-
-// masked input tel
-
 $(function() {
   $("#phone").mask("+7(999) 999-99-99");
 });
-
-// burger
 
 $(document).ready(function(){
 
@@ -48,22 +27,5 @@ $(document).ready(function(){
     $('.burgerWrap').fadeToggle(0);
     $('.burgerWrap').fadeToggle('fast');
   });
-
-  // $(".burgerWrap").click(function() {
-    // $(".burger").hide(800);
-    // $(".burger").removeClass('burger').addClass("active");
-//     $("body").addClass("activ");
-//     $(".close-icon").slideToggle(1600);
-  // });
-  // $(".burgerWrap").click(function() {
-    // $(".burger").show(3200);
-    // $(".active").removeClass("active").addClass('burger');
-//     $(".close-icon").slideToggle(1600);
-  // });
-  // $(".nav-links").click(function() {
-    // $(".burger").show(3200);
-    // $(".active").removeClass("active").addClass('burger');
-//     $(".close-icon").slideToggle(1600);
-  // });
   
 });
